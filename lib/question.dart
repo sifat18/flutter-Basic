ac7e769747ed2bb41b0875fe42cc51425cc8a173
Sifat <ibnsifat@gmail.com>
2021-07-05 04:44:19 +0600

    styling

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questex;
+  final String questex;
   Question(this.questex);
   // const Question({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questex);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(20),
+      child: Text(
+        questex,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
